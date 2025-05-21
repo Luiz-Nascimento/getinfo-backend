c7d1a855ef019016823257978d256168766df23f
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-05-21 12:21:42 -0300

    Criação de Entidade, Repository e Service de usuário

---
+++ contratos/src/main/java/com/getinfo/contratos/service/UsuarioService.java
@@ -0,0 +1,37 @@
+package com.getinfo.contratos.service;
+
+import com.getinfo.contratos.entity.Usuario;
+import com.getinfo.contratos.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> buscarPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deletar(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+
+
+}
+
+
+
