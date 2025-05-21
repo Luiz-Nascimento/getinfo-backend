902565b352c13cb4c81061541c26990726cd1634
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-05-21 17:15:42 -0300

    Adicionado login

--- contratos/src/main/java/com/getinfo/contratos/controller/AuthController.java
+++ contratos/src/main/java/com/getinfo/contratos/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.getinfo.contratos.controller;
 
+import com.getinfo.contratos.DTOs.LoginRequest;
 import com.getinfo.contratos.DTOs.UsuarioRegistroDTO;
 import com.getinfo.contratos.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +20,28 @@ public class AuthController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/register")
     public ResponseEntity<?> registrar(@RequestBody UsuarioRegistroDTO dto) {
         usuarioService.registrarNovoUsuario(dto);
         return ResponseEntity.ok("Usuário registrado com sucesso!");
     }
 
-}
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            UsernamePasswordAuthenticationToken authToken =
+                    new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password());
+
+            Authentication authentication = authenticationManager.authenticate(authToken);
+
+
+            return ResponseEntity.ok("Login realizado com sucesso!");
+        } catch (Exception e) {
+            return ResponseEntity.status(401).body("Usuário ou senha inválidos");
+        }
+    }
+    }
+
