d0286c6f160e0c378e3e3eb7181982b88d741195
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-06-10 00:47:52 -0300

    Bora

--- contratos/src/main/java/com/getinfo/contratos/service/ContratoService.java
+++ contratos/src/main/java/com/getinfo/contratos/service/ContratoService.java
@@ -19,7 +19,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class ContratoService {
