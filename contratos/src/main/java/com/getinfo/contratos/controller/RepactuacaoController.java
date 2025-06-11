c4afabb22ec0bfd054a74707f8e3d0fdb5f42988
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-06-11 13:34:47 -0300

    Download, visualização e upload de anexo de repactuação

--- contratos/src/main/java/com/getinfo/contratos/controller/RepactuacaoController.java
+++ contratos/src/main/java/com/getinfo/contratos/controller/RepactuacaoController.java
@@ -4,11 +4,11 @@ import com.getinfo.contratos.DTOs.RepactuacaoExibirDTO;
 import com.getinfo.contratos.entity.Repactuacao;
 import com.getinfo.contratos.service.RepactuacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -29,4 +29,36 @@ public class RepactuacaoController {
         RepactuacaoExibirDTO repactuacaoExibirDTO = repactuacaoService.findById(id);
         return ResponseEntity.ok().body(repactuacaoExibirDTO);
     }
+
+    @GetMapping("/view/{id}")
+    public ResponseEntity<byte[]> viewAnexo(@PathVariable Long id) {
+        byte[] anexo = repactuacaoService.obterAnexo(id);
+        if (anexo == null || anexo.length == 0) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                        "inline; filename=anexoRepactuacao.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(anexo);
+    }
+
+    @GetMapping("/download/{id}")
+    public ResponseEntity<byte[]> downloadAnexo(@PathVariable Long id) {
+        byte[] anexo = repactuacaoService.obterAnexo(id);
+        if (anexo == null || anexo.length == 0) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=anexoRepactuacao.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(anexo);
+    }
+    @PostMapping(value = "/upload/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> uploadAnexo(@PathVariable Long id, @RequestParam("anexo") MultipartFile anexo) {
+        repactuacaoService.uploadAnexo(id, anexo);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
 }
--- contratos/src/main/java/com/getinfo/contratos/service/RepactuacaoService.java
+++ contratos/src/main/java/com/getinfo/contratos/service/RepactuacaoService.java
@@ -1,13 +1,17 @@
 package com.getinfo.contratos.service;
 
 import com.getinfo.contratos.DTOs.RepactuacaoExibirDTO;
+import com.getinfo.contratos.entity.Aditivo;
 import com.getinfo.contratos.entity.Repactuacao;
 import com.getinfo.contratos.mappers.RepactuacaoMapper;
 import com.getinfo.contratos.repository.RepactuacaoRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -33,6 +37,30 @@ public class RepactuacaoService {
         Repactuacao repactuacao = repactuacaoRepository.findById(id)
                 .orElseThrow(()-> new EntityNotFoundException("Repactuação não encontrada."));
         return repactuacaoMapper.toDto(repactuacao);
+    }
+
+    public byte[] obterAnexo(Long id) {
+        Repactuacao repactuacao = repactuacaoRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Repactuação não encontrada."));
+        return repactuacao.getAnexo();
+    }
+
+    @Transactional
+    public void uploadAnexo(Long id, MultipartFile anexo) {
+        Repactuacao repactuacao = repactuacaoRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Repactuacao não encontrada"));
+
+        if (anexo == null || anexo.isEmpty()) {
+            throw new IllegalArgumentException("Arquivo não enviado ou está vazio");
+        }
+        if (!"application/pdf".equals(anexo.getContentType())) {
+            throw new IllegalArgumentException("Tipo de arquivo não suportado. Envie um PDF.");
+        }
+        try {
+            repactuacao.setAnexo(anexo.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao ler o arquivo enviado", e);
+        }
     }
 
 
