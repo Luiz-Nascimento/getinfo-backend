d6965044931bc5d1bd13eda42a818bab80a4da24
Luiz-Nascimento <luiznascimento.developer@gmail.com>
2025-06-13 13:39:43 -0300

    Documentação de endpoints de entregavel

--- contratos/src/main/java/com/getinfo/contratos/controller/EntregavelController.java
+++ contratos/src/main/java/com/getinfo/contratos/controller/EntregavelController.java
@@ -2,6 +2,8 @@ package com.getinfo.contratos.controller;
 
 import com.getinfo.contratos.DTOs.EntregavelExibirDTO;
 import com.getinfo.contratos.service.EntregavelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +13,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/entregaveis")
+@Tag(name = "Entregáveis", description = "Endpoints para entregável")
 public class EntregavelController {
 
     @Autowired
     private EntregavelService entregavelService;
 
+    @Operation(summary = "Lista todos entregáveis cadastrados no sistema")
     @GetMapping
     public List<EntregavelExibirDTO> findAll() {
         return entregavelService.findAll();
